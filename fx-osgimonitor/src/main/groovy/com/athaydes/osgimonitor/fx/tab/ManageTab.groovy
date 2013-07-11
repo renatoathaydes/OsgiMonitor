a48e3fa30dfe5810900c2ef7fc7bf81c69cd9ee7
Renato Athaydes <renatoathaydes@hotmail.com>
2013-07-11 19:59:43 +0200

    More improvements to Manage Tab

--- fx-osgimonitor/src/main/groovy/com/athaydes/osgimonitor/fx/tab/ManageTab.groovy
+++ fx-osgimonitor/src/main/groovy/com/athaydes/osgimonitor/fx/tab/ManageTab.groovy
@@ -1,9 +1,11 @@
 package com.athaydes.osgimonitor.fx.tab
 
+import javafx.application.Platform
 import javafx.event.ActionEvent
 import javafx.event.EventHandler
 import javafx.scene.Node
 import javafx.scene.control.Button
+import javafx.scene.control.Tooltip
 import javafx.scene.layout.VBox
 import javafx.stage.FileChooser
 
@@ -30,17 +32,28 @@ class ManageTab extends AsTab {
 	def createBundleChooserGroup( Node root ) {
 		final chooseFileButton = new Button(
 				id: 'select-bundle-button',
-				text: 'Select a bundle to be installed into the OSGi environment' )
-		final fileChooser = createJarFileChooser()
+				text: 'Install local bundle' )
+
+		final chooseRemoteButton = new Button(
+				id: 'select-remote-button',
+				text: 'Install remote bundle' )
+
+		Platform.runLater {
+			chooseFileButton.tooltip = new Tooltip(
+					'Select a local bundle to be installed into the OSGi environment' )
+			chooseRemoteButton.tooltip = new Tooltip(
+					'Select a remote bundle to be installed into the OSGi environment' )
+		}
 
 		chooseFileButton.onAction = [
 				handle: { ActionEvent event ->
+					final fileChooser = createJarFileChooser()
 					File f = fileChooser.showOpenDialog( root.scene.window )
 					println "You selected file ${f?.absolutePath}"
 				}
 		] as EventHandler
 
-		return [ chooseFileButton ]
+		return [ chooseFileButton, chooseRemoteButton ]
 	}
 
 	protected createJarFileChooser( ) {
